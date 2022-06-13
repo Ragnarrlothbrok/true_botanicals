0ebeca2d9ccfc38a06070d59d369a25b36d59637
IndianRenters <indianrenters@IndianRenterss-MacBook-Pro.local>
2022-06-14 02:36:55 +0530

    Section 1 layout created

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,17 @@
-import '../styles/index.scss';
-import '../styles/navbar.scss';
-import './navbar';
+import "../styles/index.scss";
+import "../styles/navbar.scss";
+import "../styles/section2.scss";
+import "./navbar";
 
 // console.log('webpack starterkit');
 
-
+function run() {
+  var password = prompt("Password Please");
+  if (password != "anatta") {
+    document.body.innerHTML = "";
+    document.body.innerHTML = "Password Failed! Reload to Renter Password";
+  } else {
+    alert("Success");
+  }
+}
+run();
