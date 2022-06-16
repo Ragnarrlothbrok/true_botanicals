498f64d64c1ef3338a4ae49f1d2c52314c74baaa
IndianRenters <indianrenters@IndianRenterss-MacBook-Pro.local>
2022-06-16 16:19:34 +0530

    Section 3 layout created

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,7 @@ import "../styles/index.scss";
 import "../styles/navbar.scss";
 import "../styles/section2.scss";
 import "../styles/section3.scss";
+import "../styles/section4.scss";
 import "./navbar";
 import "./section2";
 
