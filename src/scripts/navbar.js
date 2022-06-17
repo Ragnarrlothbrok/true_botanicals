321794bc68662b0b1eca1b58abc25004ab9b5592
IndianRenters <indianrenters@IndianRenterss-MacBook-Pro.local>
2022-06-17 05:32:30 +0530

    Navbar layout adjusted

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -1,41 +1,42 @@
-window.onscroll = function() {
-    scrollFunction();
-    stickyFunction();
+window.onscroll = function () {
+  scrollFunction();
 };
 
 let navbar = document.getElementById("navbar");
-let sticky = navbar.offsetTop;
 const hamburgerMenu = document.getElementById("menuIcon");
-const overlay = document.querySelector('.overlay');
-const navbarRight = document.getElementById('navbar-right');
-
-function stickyFunction() {
-    if (window.pageYOffset >= sticky) {
-      navbar.classList.add("sticky");
-    } else {
-      navbar.classList.remove("sticky");
-    }
-  }
-
+const overlay = document.querySelector(".overlay");
+const navbarRight = document.getElementById("navbar-right");
+const dropdown = document.querySelectorAll(".dropdown");
 
 function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
     navbar.style.height = "58px";
     document.getElementById("navbar-right").style.lineHeight = "58px";
+    document.getElementById("navbar-right").style.top = "58px";
     document.getElementById("navbar-left").style.lineHeight = "58px";
   } else {
     navbar.style.height = "99px";
     document.getElementById("navbar-right").style.lineHeight = "99px";
+    document.getElementById("navbar-right").style.top = "135px";
     document.getElementById("navbar-left").style.lineHeight = "99px";
   }
 }
 
-hamburgerMenu.addEventListener('click', ()=>{
-  navbarRight.classList.toggle('active');
-  overlay.classList.toggle('active');
+for (let i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("mouseenter", function () {
+    overlay.classList.add("active");
+  });
+  dropdown[i].addEventListener("mouseleave", function () {
+    overlay.classList.remove("active");
+  });
+}
+
+hamburgerMenu.addEventListener("click", () => {
+  navbarRight.classList.toggle("active");
+  overlay.classList.toggle("active");
 });
 
-overlay.addEventListener('click', ()=> {
-  navbarRight.classList.remove('active');
-  overlay.classList.remove('active');
+overlay.addEventListener("click", () => {
+  navbarRight.classList.remove("active");
+  overlay.classList.remove("active");
 });
