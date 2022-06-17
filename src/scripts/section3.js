54a5d83c00c5009249510af6cfa8677e145331ff
IndianRenters <indianrenters@IndianRenterss-MacBook-Pro.local>
2022-06-17 15:57:57 +0530

    Picture tags added.

--- src/scripts/section3.js
+++ src/scripts/section3.js
@@ -1,2 +1,37 @@
+import {
+  Swiper,
+  Navigation,
+  Pagination,
+  Scrollbar,
+  EffectCoverflow,
+} from "swiper";
 
+Swiper.use([Navigation, Pagination, Scrollbar, EffectCoverflow]);
 
+var swiper = new Swiper(".mySwiper", {
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1500: {
+      slidesPerView: 3,
+    },
+    1600: {
+      slidesPerView: 4,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    draggable: true,
+  },
+});
+
+swiper();
